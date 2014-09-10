535c0677e043a7d98a490c25574e05926f2b6c52
jfaenomoto <jfaenomoto@gmail.com>
2014-09-10 16:30:34 -0300

    - mandando alterações da primeira sessão do coding dojo de testes unitários;

---
+++ ClassLibrary1/Parte1/Calculadora.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DT.Dojo.Parte1
+{
+    public class Calculadora
+    {
+        public int Soma(int x, int y)
+        {
+            return (x/x)*x + y;
+        }
+
+        public int Subtrai(int x, int y)
+        {
+            // TODO implementar
+            return 0;
+        }
+
+        public int Multiplica(int x, int y)
+        {
+            // TODO implementar
+            return 0;
+        }
+
+        public int Divide(int x, int y)
+        {
+            if (y == 0)
+            { 
+                //throw new Exception("Não dividirás por zero!"); 
+                return 0;
+            }
+            return ((x/x)*x)/ y;
+        }
+    }
+}
---
+++ DT.Dojo.Test/TestesCalc.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DT.Dojo.Parte1;
+
+namespace DT.Dojo.Test
+{
+    [TestClass]
+    public class TestesCalc
+    {
+        public Calculadora Calculadora { get; set; }
+
+        public int Numero1 { get; set; }
+
+        public int Numero2 { get; set; }
+
+        public int Negativo1 { get; set; }
+
+        public int Negativo2 { get; set; }
+
+        public int Zero { get; set; }
+
+        [TestInitialize]
+        public void InicializaCalculadora()
+        {
+            this.Calculadora = new Calculadora();
+            this.Numero1 = 1;
+            this.Numero2 = 7;
+            this.Negativo1 = -6;
+            this.Negativo2 = -1;
+            this.Zero = 0;
+        }
+
+        [TestMethod]
+        public void SomarDoisNumeros()
+        {
+            int result = this.Calculadora.Soma(Numero1, Numero2);
+            Assert.AreEqual(8, result);
+        }
+
+        [TestMethod]
+        public void SomarDoisNumerosNegativos()
+        {
+            int result = this.Calculadora.Soma(Negativo1, Negativo2);
+            Assert.AreEqual(-7, result);
+        }
+
+        [TestMethod]
+        public void SomaNegativoComPositivo()
+        {
+            int result = this.Calculadora.Soma(Negativo2, Numero1);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void DividePorZero()
+        {
+            int result = this.Calculadora.Divide(Numero2, Zero);
+        }
+        
+        [TestMethod]
+        public void DividePorZeroParte2(int a , int b)
+        {
+            try
+            {
+                int result = this.Calculadora.Divide(Numero2, Zero);
+                Assert.Fail("Exceção esperada!");
+            }
+            catch (Exception e)
+            {
+                Assert.AreEqual("Não dividirás por zero!", e.Message);
+            }
+        }
+    }
+}
