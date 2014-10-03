c3e7497ac69b3bfbdbf57ca344594f5c9c3feb6d
jfaenomoto <jfaenomoto@gmail.com>
2014-10-03 13:01:19 -0300

    - adicionando classes para a parte 3 - mocks;

---
+++ ClassLibrary1/Parte3/IEquipeDeManutencao.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Parte3
+{
+    public interface IEquipeDeManutencao
+    {
+        void VerificaSeFogueteOK(Foguete foguete);
+    }
+}
---
+++ ClassLibrary1/Parte3/LancadorDeFoguete.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1.Parte3
+{
+    public class LancadorDeFoguete
+    {
+        private IFabricaDeFoguete Fabrica { get; set; }
+
+        private IEquipeDeManutencao Equipe { get; set; }
+
+        public void LancaFoguete()
+        {
+            // código aqui
+        }
+    }
+}
